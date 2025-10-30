11fbcd2c7f20466b3e7bd1da4a29ce896d660f57
F15 <duongquocthai368@gmail.com>
2025-10-30 13:54:20 +0700

    git init

---
+++ ThaiDQ_SE1836_A2/ThaiDQ_WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ThaiDQ_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
